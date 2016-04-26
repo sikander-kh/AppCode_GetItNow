6bbbddbe8d9adeb114512fa944f309ebe9a9d4a3
sikander <sikanderbhutto1@gmail.com>
2016-04-26 20:57:55 +0530

    first time

---
+++ app/src/main/java/com/appcode/GetItNow/Child.java
@@ -0,0 +1,26 @@
+package com.appcode.GetItNow;
+
+/**
+ * Created by appcode on 24/4/16.
+ */
+public class Child {
+
+    private String Name;
+    private int Image;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public int getImage() {
+        return Image;
+    }
+
+    public void setImage(int Image) {
+        this.Image = Image;
+    }
+}
---
+++ app/src/main/java/com/appcode/GetItNow/MainActivity.java
@@ -0,0 +1,158 @@
+package com.appcode.GetItNow;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ExpandListAdapter ExpAdapter;
+    private ArrayList<Group> ExpListItems;
+    private ExpandableListView ExpandList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ExpandList = (ExpandableListView) findViewById(R.id.exp_list);
+        ExpListItems = SetStandardGroups();
+        ExpAdapter = new ExpandListAdapter(MainActivity.this, ExpListItems);
+        ExpandList.setAdapter(ExpAdapter);
+    }
+
+    public ArrayList<Group> SetStandardGroups() {
+
+        String group_names[] = { "Lifestyle", "Entertainment", "Others"};
+
+        String country_names[] = { "Bugis: Multi Cultural", "Buffet at Landmark", "Buffet at Landmark", "Cameroon",
+                "Netherlands", "chile", "Spain", "Australia", "Colombia",
+                "Greece", "Ivory Coast", "Japan", "Costa Rica", "Uruguay",
+                "Italy", "England", "France", "Switzerland", "Ecuador",
+                "Honduras", "Agrentina", "Nigeria", "Bosnia and Herzegovina",
+                "Iran", "Germany", "United States", "Portugal", "Ghana",
+                "Belgium", "Algeria", "Russia", "Korea Republic" };
+
+        int Images[] = { R.drawable.landmark1, R.drawable.landmark1,
+                R.drawable.landmark1, R.drawable.landmark1, R.drawable.landmark1,
+                R.drawable.landmark1, R.drawable.landmark1, R.drawable.landmark1,
+                R.drawable.landmark1, R.drawable.landmark1, R.drawable.landmark1,
+                R.drawable.landmark1, R.drawable.landmark1, R.drawable.landmark1,
+                R.drawable.right1, R.drawable.right1, R.drawable.right1,
+                R.drawable.right1, R.drawable.right1,
+                R.drawable.right1, R.drawable.right1, R.drawable.right1,
+                R.drawable.right1, R.drawable.right1, R.drawable.right1,
+                R.drawable.right1, R.drawable.right1, R.drawable.right1,
+                R.drawable.right1, R.drawable.right1, R.drawable.right1,
+                R.drawable.right1 };
+
+        ArrayList<Group> list = new ArrayList<Group>();
+
+        ArrayList<Child> ch_list;
+
+        int size = 3;
+        int j = 0;
+
+        for (String group_name : group_names) {
+            Group gru = new Group();
+            gru.setName(group_name);
+
+            ch_list = new ArrayList<Child>();
+            for (; j < size; j++) {
+                Child ch = new Child();
+                ch.setName(country_names[j]);
+                ch.setImage(Images[j]);
+                ch_list.add(ch);
+            }
+            gru.setItems(ch_list);
+            list.add(gru);
+
+            size = size + 3;
+        }
+
+        return list;
+    }
+
+
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
